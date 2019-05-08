ea26c68215aa9a3e1011e32c196c1c99c8e02747
Puvis <zpvrbove@gmail.com>
2019-05-08 19:33:40 +0200

    Update generator.cs
    add doxygen comments

--- SPT_Reaction/SPT_Reaction/generator.cs
+++ SPT_Reaction/SPT_Reaction/generator.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace SPT_Reaction
 {
-    //enum pre operácie
+    /// <summary>
+    /// Enum operácie pre príklad.
+    /// </summary>
     public enum Operation
     {
         add = '+',
@@ -14,23 +16,33 @@ namespace SPT_Reaction
         multiply = '*',
         divide = '/'
     }
-    //trieda príkladu
+    /// <summary>
+    /// Trieda Príkladu.
+    /// </summary>
     public class Problem
     {
 
-        public Operation operation { get; set; }
-        public int a { get; set; }
-        public int b { get; set; }
-        public int result { get; set; }
-        public static Random rand { get; private set; } = new Random();
+        public Operation operation { get; set; } //!< Enum operácie pre príklad
+        public int a { get; set; } //!< Prvá hodnota príkladu
+        public int b { get; set; } //!< Druhá hodnota príkladu
+        public int result { get; set; } //!< Výsledok operácie 
+        public static Random rand { get; private set; } = new Random(); //!< Funkica Random
         
-        //funkcia pre vrátenie random operácie pre príklad
+        /// <summary>
+        /// Funkcia RandomOperation vráti náhodnú operáciu pre príklad.
+        /// </summary>
+        /// <returns>Enum s danou náhodne vybranou operáciou</returns>
         public static T RandomOperation<T>()
         {
             Array enumValues = Enum.GetValues(typeof(T));
             return (T)enumValues.GetValue(rand.Next(enumValues.Length));
         }
 
+        /// <summary>
+        /// Inicializátor objektu problém, ktorý vytvorí dve náhodné čísla, zvolí pre ne operáciu
+        /// a vypočíta výsledok pre spätnú kontrolu.
+        /// </summary>
+        /// <returns>Nový objekt problému</returns>
         public Problem()
         {
             //generovanie random čísel pre premenné pre zabránenie celočíselnému deleniu a deleniu nulou
